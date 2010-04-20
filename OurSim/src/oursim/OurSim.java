177eaabc7ae531790b9a3171f91edc3666c53d8b
matheusgr <matheusgr@34847dc8-a830-5e7c-44ea-affb9cf5c607>
2010-04-20 18:44:10 +0000

    Separating CLI from API.

--- OurSim/src/oursim/OurSim.java
+++ OurSim/src/oursim/OurSim.java
@@ -7,15 +7,10 @@ import org.apache.commons.lang.time.StopWatch;
 
 import oursim.entities.Job;
 import oursim.entities.Peer;
-import oursim.events.EventQueue;
 import oursim.events.FinishJobEvent;
 import oursim.input.SyntheticWorkload;
 import oursim.input.Workload;
-import oursim.output.OutputManager;
-import oursim.output.PrintOutput;
-import oursim.policy.JobSchedulerPolicy;
 import oursim.policy.NoFSharingPolicy;
-import oursim.policy.OurGridScheduler;
 
 public class OurSim {
 
@@ -51,28 +46,6 @@ public class OurSim {
 
     }
 
-    private static void scheduleEvents(EventQueue eq, Workload workload, JobSchedulerPolicy sp) {
-
-	while (workload.peek() != null) {
-	    Job job = workload.poll();
-	    long time = job.getSubmissionTime();
-	    eq.addSubmitJobEvent(time, job, sp);
-	}
-
-    }
-
-    public static void run(EventQueue eq, JobSchedulerPolicy sp) {
-
-	while (eq.peek() != null) {
-	    long time = eq.peek().getTime();
-	    while (eq.peek() != null && eq.peek().getTime() == time) {
-		eq.poll().action();
-	    }
-	    sp.scheduleJobs();
-	}
-
-    }
-
     public static void main(String[] args) {
 
 	StopWatch stopWatch = new StopWatch();
@@ -82,16 +55,9 @@ public class OurSim {
 //	OutputManager.getInstance().addListener(new PrintOutput());
 
 	List<Peer> peers = prepareGrid(Parameters.NUM_PEERS, Parameters.PEER_SIZE);
-
 	Workload workload = prepareWorkload(peers);
 
-	EventQueue eq = new EventQueue();
-
-	JobSchedulerPolicy sp = new OurGridScheduler(eq, peers);
-
-	scheduleEvents(eq, workload, sp);
-
-	run(eq, sp);
+	OurSimAPI.run(peers, workload);
 
 	System.out.println("# Total of  finished jobs: " + FinishJobEvent.amountOfFinishedJobs);
 	System.out.println("# Total of preempted jobs: " + Job.numberOfPreemptionsForAllJobs);
