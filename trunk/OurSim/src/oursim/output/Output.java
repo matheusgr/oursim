0256fed28c6d07004a46da20354a0a6f449368bc
edigley <edigley@34847dc8-a830-5e7c-44ea-affb9cf5c607>
2010-04-20 16:28:32 +0000

    Observer pattern added to manage output events.

--- trunk/OurSim/src/oursim/output/Output.java
+++ trunk/OurSim/src/oursim/output/Output.java
@@ -1,13 +1,5 @@
 package oursim.output;
 
-import oursim.entities.Job;
-
-public interface Output {
-
-    public abstract void submitJob(long time, Job job);
-
-    public abstract void startJob(long time, Job job);
-
-    public abstract void finishJob(long time, Job job);
+public interface Output extends JobEventListener {
 
 }
--- trunk/OurSim/src/oursim/output/PrintOutput.java
+++ trunk/OurSim/src/oursim/output/PrintOutput.java
@@ -8,15 +8,13 @@ import oursim.entities.Job;
 
 public class PrintOutput implements Output {
 
-    private static Output instance;
-
     private PrintStream out;
-    
-    private PrintOutput() {
+
+    public PrintOutput() {
 	this.out = System.out;
     }
 
-    private PrintOutput(String fileName) {
+    public PrintOutput(String fileName) {
 	try {
 	    this.out = new PrintStream(new File(fileName));
 	} catch (FileNotFoundException e) {
@@ -24,27 +22,34 @@ public class PrintOutput implements Output {
 	}
     }
 
-    public synchronized static Output getInstance() {
-	if (instance == null) {
-	    instance = new PrintOutput("trace_oursim.txt");
-	}
-	return instance;
+    @Override
+    public void jobFinished(JobEvent jobEvent) {
+
+	Job job = (Job) jobEvent.getSource();
+
+	long id = job.getId();
+	long submissionTime = job.getSubmissionTime();
+	int numberOfpreemptions = job.getNumberOfpreemptions();
+	long runTimeDuration = job.getRunTimeDuration();
+
+	this.out.println("F:" + id + ":" + submissionTime + ":" + runTimeDuration + ":" + numberOfpreemptions);
+
     }
 
     @Override
-    public void finishJob(long time, Job job) {
-	this.out.println("F:" + time + ":" + ":" + job.getId() + ":" + job.getSubmissionTime() + ":" + job.getRunTimeDuration() + ":"
-		+ (time - job.getSubmissionTime() + ":" + job.getNumberOfpreemptions()));
+    public void jobSubmitted(JobEvent jobEvent) {
+
+	Job job = (Job) jobEvent.getSource();
+	this.out.println("U:" + job.getSubmissionTime() + ":" + job.getId());
+
     }
 
     @Override
-    public void submitJob(long time, Job job) {
-	this.out.println("U:" + time + ":" + job.getId());
-    }
+    public void jobStarted(JobEvent jobEvent) {
 
-    @Override
-    public void startJob(long time, Job job) {
-	this.out.println("S:" + time + ":" + job.getId());
+	Job job = (Job) jobEvent.getSource();
+	this.out.println("S:" + job.getStartTime() + ":" + job.getId());
+
     }
 
 }
