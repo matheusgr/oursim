fe112f84326ef26db0d853b36d42e39198790336
edigley <edigley@34847dc8-a830-5e7c-44ea-affb9cf5c607>
2010-06-01 15:26:40 +0000

    javadocs of the new packages in simulationevents.

--- OurSim/src/br/edu/ufcg/lsd/oursim/simulationevents/ActiveEntity.java
+++ OurSim/src/br/edu/ufcg/lsd/oursim/simulationevents/ActiveEntity.java
@@ -1,11 +1,31 @@
 package br.edu.ufcg.lsd.oursim.simulationevents;
 
+/**
+ * 
+ * This is a convenient interface to state all the entity that might alter the
+ * eventQueue or want to know what is the current time of the simulation. This
+ * interface should be used carefully and only by entities that really need it.
+ * 
+ * @author Edigley P. Fraga, edigley@lsd.ufcg.edu.br
+ * @since 01/06/2010
+ * 
+ */
 public interface ActiveEntity {
-	
+
+	/**
+	 * Set in this entity the event queue. All the entitys must share the same
+	 * instance of the eventqueue to maintain consistency.
+	 * 
+	 * @param eventQueue
+	 *            the eventqueue to be shared.
+	 */
 	void setEventQueue(EventQueue eventQueue);
 
+	/**
+	 * @return the active eventqueue.
+	 */
 	EventQueue getEventQueue();
-	
+
 	long getCurrentTime();
 
 }
--- OurSim/src/br/edu/ufcg/lsd/oursim/simulationevents/package-info.java
+++ OurSim/src/br/edu/ufcg/lsd/oursim/simulationevents/package-info.java
@@ -1,7 +1,7 @@
 /**
  * Contains the events that drive the simulation. 
  * 
- * The method {@link TimedEvent#action()} of each event is this package is intended to perform 
+ * The method {@link TimedEvent#action()} of each event is this package and its subpackages is intended to perform 
  * some action specifically related with its equivalent class in package {@link br.edu.ufcg.lsd.oursim.entities} 
  * and dispatch the appropriate event of the package {@link br.edu.ufcg.lsd.oursim.dispatchableevents} that will
  * be treaded by all the registered listeners. 
